cb1c1fe3607efabcc34cecf8c879e9a2a4eafae1
Natasha Kelly <Natasha.Kelly@student.uts.edu.au>
2022-08-22 15:43:58 +1000

    90 percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     public int i;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
